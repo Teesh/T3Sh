b10bb1f6963be34a1ec82ebb3cc0a5a10ede2226
Teesh <teesh.s93@gmail.com>
2021-02-10 12:43:43 -0600

    fix cleaner for undefineds

--- app/cleaner.js
+++ app/cleaner.js
@@ -6,34 +6,63 @@ import mongo from "../db/connect.js"
 
 export default async function (bot) {
     let db = await mongo.db()
-    let channel = bot.channels.cache.find(c => c.name.toLowerCase().includes(settings.default_calendar_channel))
+    let events = bot.channels.cache.find(c => c.name.toLowerCase().includes(settings.default_calendar_channel))
+    let polls = bot.channels.cache.find(c => c.name.toLowerCase().includes(settings.default_poll_channel))
 
     db.collection("events").find().forEach(async (event) => {
-        if (moment().diff(moment(event.expire, 'dddd MMM Do, h:mm a')) > 0) {
+        if (event.expire === "Invalid date" || moment().diff(moment(event.expire, 'dddd MMM Do, h:mm a')) > 0) {
             let message
             console.log(moment().format(), `cleaning event: ${event._id}`)
             try {
-                message = await channel.messages.cache.find(m => m.id === event._id)
+                let messages = await events.messages.fetch()
+                message = messages.find(m => m.id === event._id)
             } catch (e) {
                 console.error(e)
             } finally {
                 if (message) message.delete()
                 db.collection("events").deleteOne({ _id: event._id })
             }
+        } else {
+            let message
+            try {
+                let messages = await events.messages.fetch()
+                message = messages.find(m => m.id === event._id)
+            } catch (e) {
+                console.error(e)
+            } finally {
+                if (message && message.deleted || !message) {
+                    console.log(moment().format(), `cleaning event: ${event._id}`)
+                    db.collection("events").deleteOne({ _id: event._id })
+                }
+            }
         }
     })
 
     db.collection("polls").find().forEach(async (poll) => {
-        if (moment().diff(moment(poll.expire, 'dddd MMM Do, h:mm a')) > 0) {
+        if (poll.expire === "Invalid date" || moment().diff(moment(poll.expire, 'dddd MMM Do, h:mm a')) > 0) {
             let message
             console.log(moment().format(), `cleaning poll: ${poll._id}`)
             try {
-                message = await channel.messages.cache.find(m => m.id === poll._id)
+                let messages = await polls.messages.fetch()
+                message = messages.find(m => m.id === poll._id)
             } catch (e) {
                 console.error(e)
             } finally {
                 if (message) message.delete()
                 db.collection("polls").deleteOne({ _id: poll._id })
+            }
+        } else {
+            let message
+            try {
+                let messages = await polls.messages.fetch()
+                message = messages.find(m => m.id === poll._id)
+            } catch (e) {
+                console.error(e)
+            } finally {
+                if (message && message.deleted || !message) {
+                    console.log(moment().format(), `cleaning poll: ${poll._id}`)
+                    db.collection("polls").deleteOne({ _id: poll._id })
+                }
             }
         }
     })
