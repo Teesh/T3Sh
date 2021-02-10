584a79e5ee0d8e631544afba25af1ee2902b124c
Teesh <teesh.s93@gmail.com>
2021-02-10 13:01:23 -0600

    add debug messages

--- app/cleaner.js
+++ app/cleaner.js
@@ -10,6 +10,7 @@ export default async function (bot) {
     let polls = bot.channels.cache.find(c => c.name.toLowerCase().includes(settings.default_poll_channel))
 
     db.collection("events").find().forEach(async (event) => {
+        console.log(event.name, moment().diff(moment(event.expire, 'dddd MMM Do, h:mm a')))
         if (event.expire === "Invalid date" || moment().diff(moment(event.expire, 'dddd MMM Do, h:mm a')) > 0) {
             let message
             console.log(moment().format(), `cleaning event: ${event._id}`)
@@ -39,6 +40,7 @@ export default async function (bot) {
     })
 
     db.collection("polls").find().forEach(async (poll) => {
+        console.log(poll.name, moment().diff(moment(poll.expire, 'dddd MMM Do, h:mm a')))
         if (poll.expire === "Invalid date" || moment().diff(moment(poll.expire, 'dddd MMM Do, h:mm a')) > 0) {
             let message
             console.log(moment().format(), `cleaning poll: ${poll._id}`)
