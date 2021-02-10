7fe935e2e4ea393b078eac95b8ce4c2c12967dc0
Teesh <teesh.s93@gmail.com>
2021-02-10 13:10:55 -0600

    walk back breaking errors

--- app/cleaner.js
+++ app/cleaner.js
@@ -10,10 +10,9 @@ export default async function (bot) {
     let polls = bot.channels.cache.find(c => c.name.toLowerCase().includes(settings.default_poll_channel))
 
     db.collection("events").find().forEach(async (event) => {
-        console.log(event.name, moment().diff(moment(event.expire, 'dddd MMM Do, h:mm a')))
         if (event.expire === "Invalid date" || moment().diff(moment(event.expire, 'dddd MMM Do, h:mm a')) > 0) {
             let message
-            console.log(moment().format(), `cleaning event: ${event._id}`)
+            console.log(moment().format(), `cleaning event: ${event.name}`)
             try {
                 let messages = await events.messages.fetch()
                 message = messages.find(m => m.id === event._id)
@@ -23,27 +22,13 @@ export default async function (bot) {
                 if (message) message.delete()
                 db.collection("events").deleteOne({ _id: event._id })
             }
-        } else {
-            let message
-            try {
-                let messages = await events.messages.fetch()
-                message = messages.find(m => m.id === event._id)
-            } catch (e) {
-                console.error(e)
-            } finally {
-                if (message && message.deleted || !message) {
-                    console.log(moment().format(), `cleaning event: ${event._id}`)
-                    db.collection("events").deleteOne({ _id: event._id })
-                }
-            }
         }
     })
 
     db.collection("polls").find().forEach(async (poll) => {
-        console.log(poll.name, moment().diff(moment(poll.expire, 'dddd MMM Do, h:mm a')))
         if (poll.expire === "Invalid date" || moment().diff(moment(poll.expire, 'dddd MMM Do, h:mm a')) > 0) {
             let message
-            console.log(moment().format(), `cleaning poll: ${poll._id}`)
+            console.log(moment().format(), `cleaning poll: ${poll.name}`)
             try {
                 let messages = await polls.messages.fetch()
                 message = messages.find(m => m.id === poll._id)
@@ -52,19 +37,6 @@ export default async function (bot) {
             } finally {
                 if (message) message.delete()
                 db.collection("polls").deleteOne({ _id: poll._id })
-            }
-        } else {
-            let message
-            try {
-                let messages = await polls.messages.fetch()
-                message = messages.find(m => m.id === poll._id)
-            } catch (e) {
-                console.error(e)
-            } finally {
-                if (message && message.deleted || !message) {
-                    console.log(moment().format(), `cleaning poll: ${poll._id}`)
-                    db.collection("polls").deleteOne({ _id: poll._id })
-                }
             }
         }
     })
